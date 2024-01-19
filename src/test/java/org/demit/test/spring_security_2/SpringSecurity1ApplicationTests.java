52c97fc4de3d7e2d08cb91482b5c9964c9c6fc40
danieler79 <daniel.erneholm@gmail.com>
2024-01-19 09:36:08 +0100

    Initial commit, with added default security.

---
+++ src/test/java/org/demit/test/spring_security_2/SpringSecurity1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demit.test.spring_security_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
